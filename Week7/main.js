cdd7876706464682b44a25a2ec56b44eed243544
Cayden R. Haun <floridaman7588@gmail.com>
2025-04-22 21:58:26 -0500

    week 7 project
    :333 silly silly silly
    i wonder if anyone is reading these

---
+++ Week7/main.js
@@ -0,0 +1,17 @@
+let userName = prompt("Enter your name");
+
+function calcGasAvg() {
+	let total = 0;
+	let count = 0;
+	let weeklyGas = prompt("Enter your weekly gas total. Enter -1 when you are finished");
+
+	while (weeklyGas != -1) {
+		total += +weeklyGas;
+		count++;
+		weeklyGas = prompt("Enter your weekly gas total. Enter -1 when you are finished");
+	}
+	return total / count;
+}
+
+let gasAvg = calcGasAvg();
+prompt(userName + "'s average weekly gas bill is $" + gasAvg.toFixed(2));
